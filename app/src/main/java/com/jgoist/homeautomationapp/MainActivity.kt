a487b2420a3f45c9ce045eadedb7a932fcd1a8f1
Brooke Brown <brooke_brown23@aol.com>
2019-04-10 22:07:38 -0400

    updating widget and adding main app page

--- app/src/main/java/com/jgoist/homeautomationapp/MainActivity.kt
+++ app/src/main/java/com/jgoist/homeautomationapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.jgoist.homeautomationapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.AutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val mEmailView: AutoCompleteTextView? = null
+    private val mPasswordView: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
